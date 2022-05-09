5bda121d6565331488132c9d9aa5252452e0602d
Mateus <mbaraoengenharia@gmail.com>
2022-05-09 10:12:40 -0300

    populando tabelas

--- database/seeders/20220504141918-seed-usuarios.js
+++ database/seeders/20220504141918-seed-usuarios.js
@@ -1,24 +1,33 @@
 'use strict';
 
+const bcrypt = require ('bcrypt');
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+
+  await queryInterface.bulkInsert('usuarios', [
+      {
+      id: 1,
+      nome: 'Moraes',
+      email: "moraes@email.com",
+      senha: bcrypt.hashSync('123456', 10),
+      createdAt: new Date().toISOString().substring(0, 19),
+      updatedAt: new Date().toISOString().substring(0, 19)
+    },
+
+    {
+      id: 2,
+      nome: 'Mariana',
+      email: "mariana@email.com",
+      senha: bcrypt.hashSync('123456', 10),
+      createdAt: new Date().toISOString().substring(0, 19),
+      updatedAt: new Date().toISOString().substring(0, 19)
+    }
+
+  ], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('usuarios', null, {});
   }
 };
